63b07900650d99f8717cc7f57c4c94381dcef0e6
adogbeji <baka_1993@yahoo.co.uk>
2024-03-12 20:46:39 +0000

    Add SVG icons to buyers main screen, remove DEBUG banner & remove status bar

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -15,42 +16,32 @@ class _MainScreenState extends State<MainScreen> {
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.black,
         selectedItemColor: Colors.yellow.shade900,
-        items: const [
-          BottomNavigationBarItem(
+        items: [
+          const BottomNavigationBarItem(
             icon: Icon(
               CupertinoIcons.home,
             ),
             label: 'HOME',
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.home,
-            ),
-            label: 'HOME',
+            icon: SvgPicture.asset('assets/icons/explore.svg', width: 20,),
+            label: 'CATEGORIES',
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.home,
-            ),
-            label: 'HOME',
+            icon: SvgPicture.asset('assets/icons/shop.svg', width: 20,),
+            label: 'STORE',
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.home,
-            ),
-            label: 'HOME',
+            icon: SvgPicture.asset('assets/icons/cart.svg'),
+            label: 'CART',
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.home,
-            ),
-            label: 'HOME',
+            icon: SvgPicture.asset('assets/icons/search.svg'),
+            label: 'SEARCH',
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.home,
-            ),
-            label: 'HOME',
+            icon: SvgPicture.asset('assets/icons/account.svg'),
+            label: 'ACCOUNT',
           ),
         ],
       ),
