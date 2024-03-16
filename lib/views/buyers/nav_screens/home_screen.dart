f1e3e3c031b2d174234a64888f419e68bb590cb7
adogbeji <baka_1993@yahoo.co.uk>
2024-03-16 20:26:45 +0000

    Start building buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Screen'),
+    return Padding(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+        left: 25,
+        right: 15,
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'What are you looking for? 👀',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(
+            child: SvgPicture.asset(
+              'assets/icons/cart.svg',
+              width: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
