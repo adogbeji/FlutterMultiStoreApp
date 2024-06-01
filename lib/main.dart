a21b642bd13a36892a2a235fb036c7c85d963192
adogbeji <baka_1993@yahoo.co.uk>
2024-06-01 10:01:13 +0100

    Set currentIndex & onTap properties in bottom navigation bar of buyers main screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Brand-Bold',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
