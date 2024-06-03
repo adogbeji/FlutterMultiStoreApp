01e66e143ded07268cd993392c728959761105e3
adogbeji <baka_1993@yahoo.co.uk>
2024-06-03 09:47:29 +0100

    Add search bar to buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -13,19 +13,40 @@ class HomeScreen extends StatelessWidget {
         left: 25,
         right: 15,
       ),
-      child: Row(
+      child: Column(
         children: [
-          const Text(
-            'What Are You Looking For? 👀',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'What Are You Looking For? 👀',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                child: SvgPicture.asset(
+                  'assets/icons/cart.svg',
+                  width: 20,
+                ),
+              ),
+            ],
           ),
-          Container(
-            child: SvgPicture.asset(
-              'assets/icons/cart.svg',
-              width: 20,
+
+          // SEARCH BAR 
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: const TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: 'Search For Products...',
+                hintText: 'Search Here',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+              ),
             ),
           ),
         ],
