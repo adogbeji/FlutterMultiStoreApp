e898875d0979723d46567b7d7422b37901f9c415
adogbeji <baka_1993@yahoo.co.uk>
2024-06-07 10:03:32 +0100

    Start building BannerWidget & import --> buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -2,28 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:multi_store/views/buyers/nav_screens/widgets/welcome_text_widget.dart';
 import 'package:multi_store/views/buyers/nav_screens/widgets/search_input_widget.dart';
+import 'package:multi_store/views/buyers/nav_screens/widgets/banner_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: MediaQuery.of(context).padding.top,
-        left: 25,
-        right: 15,
-      ),
-      child: const Column(
-        children: [
-          WelcomeText(),
-
-          SizedBox(height: 14,),
-
-          // SEARCH BAR 
-          SearchInputWidget(),
-        ],
-      ),
+    return const Column(
+      children: [
+        WelcomeText(),
+    
+        SizedBox(height: 14,),
+    
+        // SEARCH BAR 
+        SearchInputWidget(),
+        BannerWidget(),
+      ],
     );
   }
 }
