d7a8b441cd0ac7e9343a7c296cf8ce271be054da
adogbeji <baka_1993@yahoo.co.uk>
2024-06-10 09:37:10 +0100

    Start displaying categories in CategoryText widget

--- lib/views/buyers/nav_screens/widgets/category_text.dart
+++ lib/views/buyers/nav_screens/widgets/category_text.dart
@@ -4,25 +4,47 @@ import 'package:flutter/widgets.dart';
 class CategoryText extends StatelessWidget {
   // const CategoryText({super.key});
 
-  final List<String> _categoryLabels = ['FOOD', 'VEGETABLE', 'EGG', 'TEA'];
+  final List<String> _categoryLabels = ['FOOD', 'VEGETABLE', 'EGG', 'TEA', 'DRINKS', 'DIY', 'HOME DECOR', 'STATIONERY'];
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(9.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('Categories', style: TextStyle(fontSize: 19,),),
-
+          const Text(
+            'Categories',
+            style: TextStyle(
+              fontSize: 19,
+            ),
+          ),
           Container(
             height: 40,
             child: Row(
               children: [
                 Expanded(
-                  child: ListView.builder(itemBuilder: (context, index) {
-                    return ActionChip(label: Text(_categoryLabels[index]),);
-                  }),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _categoryLabels.length,
+                    itemBuilder: (context, index) {
+                      return ActionChip(
+                        backgroundColor: Colors.yellow.shade900,
+                        onPressed: () {},
+                        label: Text(
+                          _categoryLabels[index],
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
+
+                IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_forward),),
               ],
             ),
           ),
