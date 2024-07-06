544f39e5403c6f7daa8d58d15e7ab3cc70555769
adogbeji <baka_1993@yahoo.co.uk>
2024-07-06 09:52:42 +0100

    Add register button to register screen

--- lib/views/buyers/auth/register_screen.dart
+++ lib/views/buyers/auth/register_screen.dart
@@ -10,16 +10,76 @@ class RegisterScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Create Account', style: TextStyle(fontSize: 20,),),
+            const Text(
+              'Create Account',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
 
             CircleAvatar(
               radius: 64,
               backgroundColor: Colors.yellow.shade900,
             ),
 
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+            // EMAIL INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+
+            // NAME INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+            ),
+
+            // PHONE NUMBER INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                ),
+              ),
+            ),
+
+            // PASSWORD INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+
+            // REGISTER BUTTON
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 19,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
               ),
             ),
           ],
