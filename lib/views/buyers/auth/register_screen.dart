454cba256bc07cb5c021b8464941bda1e5aebb38
adogbeji <baka_1993@yahoo.co.uk>
2024-07-08 09:17:06 +0100

    Add login prompt button to register screen

--- lib/views/buyers/auth/register_screen.dart
+++ lib/views/buyers/auth/register_screen.dart
@@ -82,6 +82,17 @@ class RegisterScreen extends StatelessWidget {
                 ),
               ),
             ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Already have an account?'),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Log in'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
