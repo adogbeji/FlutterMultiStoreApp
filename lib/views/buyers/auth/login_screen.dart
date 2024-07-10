572e1da4ae6f8c299e2d446028db718708e27789
adogbeji <baka_1993@yahoo.co.uk>
2024-07-10 09:47:53 +0100

    Add login button & register propmt button to login screen

--- lib/views/buyers/auth/login_screen.dart
+++ lib/views/buyers/auth/login_screen.dart
@@ -10,23 +10,61 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Log In', style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),),
-
-            // EMAIL iNPUT FIELD
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
+            const Text(
+              'Log In',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
             ),
 
-            // PASSWORD iNPUT FIELD
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
+            // EMAIL INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
               ),
+            ),
+
+            // PASSWORD INPUT FIELD
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+            
+            // LOGIN BUTTON
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'Log In',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 5,
+                  ),
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Need An Account?'),
+                TextButton(onPressed: () {}, child: const Text('Register'),),
+              ],
             ),
           ],
         ),
