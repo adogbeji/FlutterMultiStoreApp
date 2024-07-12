717f779af943eff435723dd1742d578642f08f0a
adogbeji <baka_1993@yahoo.co.uk>
2024-07-12 09:53:50 +0100

    Add code to create new user account in AuthController class

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,4 +1,21 @@
-class AuthController {
+import 'package:firebase_auth/firebase_auth.dart';
 
-  Future<String> signUpUsers() {}
+class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;  // stores firebase_auth package
+
+  Future<String> signUpUsers(String email, String fullName, String phoneNumber, String password) async {
+    String res = 'Some error occurred';
+
+    try {
+      if (email.isNotEmpty && fullName.isNotEmpty && phoneNumber.isNotEmpty && password.isNotEmpty) {
+        // Create New User
+
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);  // Stores user credentials
+      } else {
+        res = 'Fields must not be empty';
+      }
+    } catch (e) {}
+
+    return res;
+  }
 }
