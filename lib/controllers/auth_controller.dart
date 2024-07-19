8b738f89201028a3a07e58f8e2b0889aa073896f
adogbeji <baka_1993@yahoo.co.uk>
2024-07-19 09:38:20 +0100

    Start adding code to create buyers collection in AuthController class

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
-  final FirebaseAuth _auth = FirebaseAuth.instance;  // stores firebase_auth package
+  final FirebaseAuth _auth = FirebaseAuth.instance;  // Stores firebase_auth package
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;  // Stores cloud_firestore package
 
   Future<String> signUpUsers(String email, String fullName, String phoneNumber, String password) async {
     String res = 'Some error occurred';
@@ -10,7 +12,18 @@ class AuthController {
       if (email.isNotEmpty && fullName.isNotEmpty && phoneNumber.isNotEmpty && password.isNotEmpty) {
         // Create New User
 
-        UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);  // Stores user credentials
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email, 
+          password: password
+        );  // Stores user credentials
+      
+        await _firestore.collection('buyers').doc(cred.user!.uid).set({
+          'email': email,
+          'fullName': fullName,
+          'phoneNumber': phoneNumber,
+        });
+
+        res = 'Success';
       } else {
         res = 'Fields must not be empty';
       }
