536fd18748ab18f017ff89ef904cf088e3250e83
adogbeji <baka_1993@yahoo.co.uk>
2024-07-26 09:43:54 +0100

    Bind form key to Form widget

--- lib/views/buyers/auth/login_screen.dart
+++ lib/views/buyers/auth/login_screen.dart
@@ -8,7 +8,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   // const LoginScreen({super.key});
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();  // Form key
-  
+
   late String email;
 
   late String password;
@@ -17,86 +17,89 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Log In',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            // EMAIL INPUT FIELD
-            Padding(
-              padding: const EdgeInsets.all(13.0),
-              child: TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Email must not be empty!';
-                  } else {
-                    return null;
-                  }
-                },
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Log In',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-
-            // PASSWORD INPUT FIELD
-            Padding(
-              padding: const EdgeInsets.all(13.0),
-              child: TextFormField(
-                validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Password must not be empty!';
-                      } else {
-                        return null;
-                      }
-                    },
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 20,),
-            
-            // LOGIN BUTTON
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.yellow.shade900,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Text(
-                  'Log In',
-                  style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 5,
+          
+              // EMAIL INPUT FIELD
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Email must not be empty!';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
                   ),
                 ),
               ),
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Need An Account?'),
-                TextButton(onPressed: () {}, child: const Text('Register'),),
-              ],
-            ),
-          ],
+          
+              // PASSWORD INPUT FIELD
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: TextFormField(
+                  validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Password must not be empty!';
+                        } else {
+                          return null;
+                        }
+                      },
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+          
+              const SizedBox(height: 20,),
+              
+              // LOGIN BUTTON
+              Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.yellow.shade900,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 5,
+                    ),
+                  ),
+                ),
+              ),
+          
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Need An Account?'),
+                  TextButton(onPressed: () {}, child: const Text('Register'),),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
