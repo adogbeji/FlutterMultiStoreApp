f8ae04a774eb6274125e9b379e85f4906895f536
adogbeji <baka_1993@yahoo.co.uk>
2024-08-19 09:18:59 +0100

    Add return button to buyes cart screen

--- lib/views/buyers/nav_screens/cart_screen.dart
+++ lib/views/buyers/nav_screens/cart_screen.dart
@@ -5,16 +5,37 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Your Shopping Cart Is Empty',
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
+                letterSpacing: 5,
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'CONTINUE SHOPPING',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ],
